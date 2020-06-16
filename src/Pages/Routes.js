bcdb9ae8f977371e482698c8865b5f21d269f643
Tan <tanamanda13@gmail.com>
2020-06-16 23:15:49 +0200

    idem clean

--- src/Pages/Routes.js
+++ src/Pages/Routes.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
-// import Register from './Register';
 import Register from './Register'
-import Register2 from './Register2'
 
 function Routes() {
   return (
