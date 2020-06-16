282cc983711c44e505cd34a1f8b0e74dac0ed4dc
Tan <tanamanda13@gmail.com>
2020-06-16 23:04:01 +0200

    register handle errors

--- src/Pages/Register2.jsx
+++ src/Pages/Register2.jsx
@@ -6,94 +6,62 @@ import validateAuth from '../utils/validateAuth';
 
 function Register2() {
   // Custom hook useForm
-  const [values, handleChange] = useForm();
+  const [ values, handleChange ] = useForm();
   const [ errorMessage, setErrorMessage ] = useState({})
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // if (emailValidator(values.email)) {
-    // if (emailValidator(values)) {
-
-    //   axios.post(`${process.env.REACT_APP_API_BASE_URL}api/user/new`,
-    //     {
-    //       passwords: {
-    //         initial: values.password,
-    //         final: values.confirmPassword,
-    //       },
-    //       lastName: values.lastname,
-    //       firstName: values.firstname,
-    //       email: values.email
-    //     }
-    //   ).then(response => {
-    
-    //     console.log('res', response)
-    //     console.log('res data', response.data)
-        
-    //   }).catch(err => {
-    //     console.log(err)
-    //   })
-    // } else {
-    //   console.log("Veuillez saisir un e-mail valide.");
-    // }
-
-    //  // Email must have "@"" and "."
-    // const emailTest = /\S+@\S+\.\S+/;
-    // // Password 
-    // const passwordTest = /^(?=.*\d)(?=.*[A-Z])[0-9a-zA-Z]{6,}$/;
-    // let errors = {};
-
-    // if ( !emailTest.test(values.email) || )
+    if (Object.keys(validateAuth(values)).length === 0 ) 
+    {
+      createAccount();
+      // console.log("Compte créé");
+    }
+    else 
+    {
+      setErrorMessage(validateAuth(values))
+    }
   }
 
-  // Email must have "@"" and "."
-  // const emailValidator = (email) => {
-  //   const re = /\S+@\S+\.\S+/;
-  //   return re.test(email);
-  // }
-
-  // Post request
-
-  const createAccount = () => {
-      axios.post(`${process.env.REACT_APP_API_BASE_URL}api/user/new`,
-        {
-          passwords: {
-            initial: values.password,
-            final: values.confirmPassword,
-          },
-          lastName: values.lastname,
-          firstName: values.firstname,
-          email: values.email
-        }
-      ).then(response => {
-    
-        console.log('res', response)
-        console.log('res data', response.data)
-        
-      }).catch(err => {
-        console.log(err)
-      })
+  // Post request to create an account
+  async function createAccount () {
+    await axios.post(`${process.env.REACT_APP_API_BASE_URL}api/user/new`,
+      {
+        passwords: {
+          initial: values.password,
+          final: values.confirmPassword,
+        },
+        lastName: values.lastname,
+        firstName: values.firstname,
+        email: values.email
+      }
+    ).then(response => {
+      console.log('res', response)
+      console.log('res data', response.data)
+    }).catch(err => {
+      console.log(err)
+    })
   }
-
 
   return (
     <div>
-      <p>ceci est le register 2</p>
+      <p>ceci est le register 2 : Se créer un compte utilisateur</p>
 
       <form action="/register" method="post" onSubmit={handleSubmit}>
         <label>Nom</label>
-        <input type="text" name="lastname" value={values.lastname || ""} onChange={handleChange} required/>
+        <input type="text" name="lastname" value={values.lastname || ""} onChange={handleChange}/>
         <label>Prénom</label>
-        <input type="text" name="firstname" value={values.firstname || ""} onChange={handleChange} required/>
+        <input type="text" name="firstname" value={values.firstname || ""} onChange={handleChange}/>
         <label>Email</label>
-        <input type="email" name="email" value={values.email || ""} onChange={handleChange} placeholder="test@example.com" required/>
+        <input type="email" name="email" value={values.email || ""} onChange={handleChange} placeholder="test@example.com"/>
         { errorMessage &&  <p>{errorMessage.email}</p>}
        
         <label>Mot de passe</label>
-        <input type="password" name="password" value={values.password || ""} onChange={handleChange} required/>
-        <p id="passwordError"></p>
+        <input type="password" name="password" value={values.password || ""} onChange={handleChange}/>
+        { errorMessage &&  <p>{errorMessage.password}</p>}
         <label>Confirmer le mot de passe</label>
-        <input type="password" name="confirmPassword" value={values.confirmPassword || ""} onChange={handleChange} required/>
+        <input type="password" name="confirmPassword" value={values.confirmPassword || ""} onChange={handleChange}/>
+        { errorMessage &&  <p>{errorMessage.confirmPassword}</p>}
         <button type="submit">S'inscrire</button>
       </form>
 
--- src/utils/validateAuth.js
+++ src/utils/validateAuth.js
@@ -1,7 +1,7 @@
 const validateAuth = (values) => {
   // Email must have "@"" and "."
   const emailTest = /\S+@\S+\.\S+/;
-  // Password 
+  // Password security test
   const passwordTest = /^(?=.*\d)(?=.*[A-Z])[0-9a-zA-Z]{6,}$/;
   let errors = {};
   // Email Errors
@@ -11,10 +11,16 @@ const validateAuth = (values) => {
     errors.email = "Invalide email adresse";
   }
   // Password Errors
-  if (!values.passwords.initial) {
+  if (!values.password) {
     errors.password = "Veuillez entrer un mot de passe";
-  } else if (!passwordTest.test(values.passwords.initial)) {
+  } else if (!passwordTest.test(values.password)) {
     errors.password = "Le mot de passe doit contenir au moins 6 lettre, une minuscule, une majuscule et un nombre";
+  }
+  // Check password = confirmPassword
+  if (!values.confirmPassword) {
+    errors.confirmPassword = "Veuillez confirmer le mot de passe";
+  } else if (values.password !== values.confirmPassword) {
+    errors.confirmPassword = "Les mots de passe que vous avez entrés ne sont pas identiques";
   }
   return errors;
 }
