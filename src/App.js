ed6af69bb36df98d5955489c813dea228563ee14
FRobaglia <felix.robaglia@gmail.com>
2020-06-23 14:18:52 +0200

    [fix] fix login redirects

--- src/App.js
+++ src/App.js
@@ -4,32 +4,31 @@ import Routes from './pages/Routes'
 import { UserContext } from './context/UserContext'
 import AuthService from './services/AuthService';
 import AxiosService from './services/AxiosService';
+import Loading from './components/molecules/loading/Loading'
 
 AxiosService.setInterceptors()
 
 function App() {
 
   const [user, setUser] = useState(null)
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
   const value = useMemo(() => ({user, setUser}), [user, setUser])
 
   useEffect(() => {
     if (AuthService.getRefreshToken() !== null) {
-      setIsLoading(true)
-
       async function persistSession() {
         await AuthService.refreshTokens()
         setUser(await AuthService.fetchUserData())
         setIsLoading(false)
       }
-
       persistSession()
     } else {
       console.log("Aucun refresh token trouvé, pas d'autologin possible.")
+      setIsLoading(false)
     }
   }, [])
 
-  if (isLoading) return 'Fetching user data... (ici, on devrait render un component Loading)'
+  if (isLoading) return <Loading />
 
   return (
     <div className="App">
---
+++ src/components/molecules/loading/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Loading() {
+  
+  return (
+    <div>Fetching user data... Ici, peut-être une animation super classe, tout en restant sobre évidemment ? 😎😎😎 </div>
+  )
+}
+
+export default Loading
