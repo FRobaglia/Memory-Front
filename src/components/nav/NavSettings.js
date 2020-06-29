b2380dcfeded2ecfac07900249061f7e7b281479
Tan <tanamanda13@gmail.com>
2020-06-29 11:46:21 +0200

    [wip] add nav in space settings

---
+++ src/components/nav/NavSettings.js
@@ -0,0 +1,16 @@
+// import React, { useContext } from 'react';
+// import { Link } from 'react-router-dom';
+// import SpaceContext from '../../context/SpaceContext';
+
+// function NavSettings() {
+//   const { space } = useContext(SpaceContext);
+//   return (
+//     <div>
+//       <nav>
+//         <Link to=`space/${}/invite`></Link>
+//       </nav>
+//     </div>
+//   );
+// }
+
+// export default NavSettings;
