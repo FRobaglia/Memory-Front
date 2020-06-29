3c82122b569c3091f8ce3f90114147c55e45f3a2
Thomas Evano <thomas-evano@hotmail.fr>
2020-06-29 18:14:12 +0200

    [feat] -Upgrade Post form -Add title,link,image and video input with button that make them appear -Add image preview when you upload

--- src/components/space/posts/postCard/PostCard.js
+++ src/components/space/posts/postCard/PostCard.js
@@ -3,18 +3,27 @@ import UserContext from '../../../../context/UserContext';
 
 function PostCard({ post, index, deletePost }) {
   const { user } = useContext(UserContext);
+  console.log(post);
   return (
     <div>
-      {post.img && <img src={post.img} alt="post img" />}
       <h1>{post.title}</h1>
       <p>{post.text}</p>
-
+      {post.images &&
+        post.images.map((image) => (
+          <img
+            src={image.url}
+            alt="post img"
+            style={{ width: '40px' }}
+            {...{ height: '40px' }}
+          />
+        ))}
+      {post.link && <a href={post.link}>Lien</a>}
       <div>
         <img
           src={post.createdBy.image.url}
+          alt="profile pic of author"
           style={{ width: '40px' }}
           {...{ height: '40px' }}
-          alt="profile pic of author"
         />
         <p>
           {post.createdBy.firstName} {post.createdBy.lastName}
--- src/pages/SpaceMemoryPage.js
+++ src/pages/SpaceMemoryPage.js
@@ -6,6 +6,7 @@ import UserContext from '../context/UserContext';
 import { useForm, toFormData } from '../utils/forms';
 import PostService from '../services/PostService';
 import PostCard from '../components/space/posts/postCard/PostCard';
+import UploadInput from '../components/UploadInput';
 
 function SpaceMemoryPage() {
   // const [spaceID, setSpaceID] = useState();
@@ -17,6 +18,13 @@ function SpaceMemoryPage() {
   const spaceLocation = useLocation();
   const { user } = useContext(UserContext);
   const [values, handleChange] = useForm();
+  const [showPostFields, setShowPostFields] = useState({
+    title: false,
+    image: false,
+    video: false,
+    link: false,
+  });
+  const [imageFile, setImageFile] = useState([]);
 
   useEffect(() => {
     getSpaceMemoryData();
@@ -41,10 +49,23 @@ function SpaceMemoryPage() {
     getSpaceMemoryData();
   }
 
-  async function deletePost(id, index) {
+  async function deletePost(id) {
     await PostService.deletePost(id);
-    // postsData.splice(index, 1);
-    // setPostsData([...postsData]);
+    getSpaceMemoryData();
+  }
+
+  function imagePreview(e) {
+    if (e.target.files.length) {
+      for (let i = 0; i < e.target.files.length; i += 1) {
+        setImageFile([
+          ...imageFile,
+          {
+            preview: URL.createObjectURL(e.target.files[i]),
+            raw: e.target.files[i],
+          },
+        ]);
+      }
+    }
   }
 
   if (spaceErrorMessage) {
@@ -60,7 +81,6 @@ function SpaceMemoryPage() {
     <div>
       <p>
         Bienvenu dans l'espace de {space.firstName} {space.lastName}
-        {console.log(spaceData.posts)}
       </p>
       {JSON.stringify(space.createdBy) === JSON.stringify(user) ? (
         <Link
@@ -75,16 +95,108 @@ function SpaceMemoryPage() {
         ''
       )}
       <form action="/" method="post" onSubmit={createPost}>
+        {showPostFields.title && (
+          <label htmlFor="title">
+            <input
+              type="text"
+              name="title"
+              id="title"
+              placeholder="titre du souvenir"
+              value={values.title || ''}
+              onChange={handleChange}
+            />
+          </label>
+        )}
         <label htmlFor="content">
-          <input
-            type="textarea"
+          <textarea
             name="text"
             id="content"
+            cols="30"
+            rows="5"
+            placeholder="ecrit un souvenir"
             value={values.text || ''}
             onChange={handleChange}
+            required
           />
         </label>
+        {imageFile.map((image) => (
+          <img
+            src={image.preview}
+            key={image.preview}
+            alt="postsimag"
+            width="100"
+            height="100"
+          />
+        ))}
+        {showPostFields.link && (
+          <label htmlFor="link">
+            <input
+              type="text"
+              id="link"
+              name="link"
+              placeholder="lien"
+              value={values.link || ''}
+              onChange={handleChange}
+            />
+          </label>
+        )}
+        {showPostFields.image && (
+          <UploadInput
+            labelText="Photo souvenir"
+            handleChange={(handleChange, imagePreview)}
+          />
+        )}
+        {showPostFields.video && (
+          <UploadInput
+            labelText="Video souvenir"
+            specificFieldName="postVideo"
+            handleChange={handleChange}
+          />
+        )}
+        <button
+          type="button"
+          style={{ display: showPostFields.title ? 'none' : 'inline-block' }}
+          onClick={() => {
+            setShowPostFields(() => {
+              return { ...showPostFields, title: true };
+            });
+          }}
+        >
+          Ajouter un titre
+        </button>
+        <button
+          type="button"
+          style={{ display: showPostFields.link ? 'none' : 'inline-block' }}
+          onClick={() =>
+            setShowPostFields(() => {
+              return { ...showPostFields, link: true };
+            })
+          }
+        >
+          Ajouter un lien
+        </button>
+        <button
+          type="button"
+          onClick={() =>
+            setShowPostFields(() => {
+              return { ...showPostFields, image: true };
+            })
+          }
+        >
+          Ajouter une image
+        </button>
+        <button
+          type="button"
+          onClick={() =>
+            setShowPostFields(() => {
+              return { ...showPostFields, video: true };
+            })
+          }
+        >
+          Ajouter une video
+        </button>
         <button type="submit">poster un souvenir</button>
+        <div>{console.log(imageFile)}</div>
       </form>
       {spaceData.posts &&
         spaceData.posts.map((post, index) => (
