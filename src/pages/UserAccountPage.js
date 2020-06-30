584c6ea27260eb1551c2a00ca8c91c1750962ea1
Thomas Evano <thomas-evano@hotmail.fr>
2020-06-30 18:22:37 +0200

    [feature] -Add navigation tab

--- src/pages/UserAccountPage.js
+++ src/pages/UserAccountPage.js
@@ -1,15 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import UserContext from '../context/UserContext';
 import SpacesContainer from '../components/space/SpacesContainer';
+import Nav from '../utils/nav';
 
 function UserAccountPage() {
   const { user } = useContext(UserContext);
+  const [spacesTab, setSpacesTab] = useState(true);
+  const [invitationsTab, setInvitationsTab] = useState(false);
+  const [accessTab, setaccessTab] = useState(false);
 
   return (
     <div>
-      <h2>{user.firstName}</h2>
-      <h2>{user.roles}</h2>
-      <SpacesContainer />
+      <div>
+        <h2>{user.firstName}</h2>
+        <h2>{user.roles}</h2>
+      </div>
+      <Nav
+        firstTabName="Espaces"
+        setFirstTab={setSpacesTab}
+        secondTabName="invitations"
+        setSecondTab={setInvitationsTab}
+        thirdTabName="demande acces"
+        setThirdTab={setaccessTab}
+      />
+      {spacesTab && <SpacesContainer />}
+      {invitationsTab && <h1>Invitations</h1>}
+      {accessTab && <h1>Demandes d'access</h1>}
     </div>
   );
 }
