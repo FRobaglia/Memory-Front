bec2cc0e6f28c18ec0599d25d1ba8e151d3e27bc
Thomas Evano <thomas-evano@hotmail.fr>
2020-07-02 13:48:54 +0200

    [feature] -Add tab package, better requestAccess & Invitations container

--- src/components/space/RequestAccessContainer.js
+++ src/components/space/RequestAccessContainer.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import SpaceService from '../../services/SpaceService';
-import SpaceCard from './spaceCard/SpaceCard';
 
 function RequestAccessContainer() {
   const [requestAccess, setRequestAccess] = useState([]);
@@ -11,12 +10,46 @@ function RequestAccessContainer() {
   async function getInvitations() {
     setRequestAccess(await SpaceService.getUserSpaces('requestAccess'));
   }
+  async function validateSubscriber(spaceId, subscriberId) {
+    await SpaceService.validateSubscriber(spaceId, subscriberId);
+  }
+
+  async function unvalidateSubscriber(spaceId, subscriberId) {
+    await SpaceService.unvalidateSubscriber(spaceId, subscriberId);
+  }
+
   return (
     <div>
       <h1>Demande D'access</h1>
       {requestAccess &&
         requestAccess.map((space) => (
-          <SpaceCard key={space.space.id} space={space.space} />
+          <div key={space.id}>
+            <h2>
+              Espace de {space.firstName} {space.lastName}
+            </h2>
+            <div>
+              {space.users.map((user) => (
+                <div key={user.id}>
+                  <p>
+                    {user.firstName} {user.lastName}
+                  </p>
+                  <span>{user.relation.text}</span>
+                  <button
+                    onClick={() => validateSubscriber(space.id, user.id)}
+                    type="button"
+                  >
+                    Accepter la demande
+                  </button>
+                  <button
+                    onClick={() => unvalidateSubscriber(space.id, user.id)}
+                    type="button"
+                  >
+                    Refuser la demande
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
         ))}
     </div>
   );
--- src/pages/UserAccountPage.js
+++ src/pages/UserAccountPage.js
@@ -1,17 +1,29 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Tabs, useTabState, Panel } from '@bumaga/tabs';
 import UserContext from '../context/UserContext';
 import SpacesContainer from '../components/space/SpacesContainer';
-import TabNav from '../components/TabNav/TabNav';
-// import TabNavButton from '../components/TabNav/TabNavButton';
 import UserInvitationContainer from '../components/space/UserInvitationsContainer';
 import RequestAccessContainer from '../components/space/RequestAccessContainer';
 
 function UserAccountPage() {
   const { user } = useContext(UserContext);
-  const [spacesTab, setSpacesTab] = useState(true);
-  const [invitationsTab, setInvitationsTab] = useState(false);
-  const [accessTab, setaccessTab] = useState(false);
+
+  // const cn = (...args) => args.filter(Boolean).join(' ');
+
+  const Tab = ({ children }) => {
+    const { isActive, onClick } = useTabState();
+
+    return (
+      <button
+        type="button"
+        // className={cn('tab', isActive && 'active')}
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    );
+  };
 
   return (
     <div>
@@ -22,18 +34,22 @@ function UserAccountPage() {
           <button type="button">Modifier mon compte</button>
         </Link>
       </div>
-      <TabNav
-        firstTabLabel="Espaces"
-        setFirstTab={setSpacesTab}
-        secondTabLabel="invitations"
-        setSecondTab={setInvitationsTab}
-        thirdTabLabel="demande acces"
-        setThirdTab={setaccessTab}
-        tabNumber="3"
-      />
-      {spacesTab && <SpacesContainer />}
-      {invitationsTab && <UserInvitationContainer />}
-      {accessTab && <RequestAccessContainer />}
+      <Tabs>
+        <div>
+          <Tab>Espaces</Tab>
+          <Tab>Invitations</Tab>
+          <Tab>Demandes d'acces</Tab>
+        </div>
+        <Panel>
+          <SpacesContainer />
+        </Panel>
+        <Panel>
+          <UserInvitationContainer />
+        </Panel>
+        <Panel>
+          <RequestAccessContainer />
+        </Panel>
+      </Tabs>
     </div>
   );
 }
